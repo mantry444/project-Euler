817a125b1b62dcc10436830e7f807628d79036bc
Rashmikanta Mantry <88504635+mantry444@users.noreply.github.com>
2023-01-10 16:32:59 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,3 +1,8 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a2 + b2 = c2
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
 for i in range (1,501):
     for j in range (1,501):
         for k in range (1,501):
